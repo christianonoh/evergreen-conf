6c668a72c2a40c12ad907d5f1e3cfefeacfdeee4
Christian Onoh <chibyk5000@gmail.com>
2024-01-09 20:02:02 +0100

    Add layout and page components

---
+++ src/app/layout.tsx
@@ -0,0 +1,103 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { siteMetadata } from "@/constants/siteMetaData";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: siteMetadata.title,
+  description: siteMetadata.description,
+  keywords: siteMetadata.keywords,
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: siteMetadata.siteUrl,
+    siteName: siteMetadata.title,
+    images: [
+      {
+        url: siteMetadata.socialBanner, // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: siteMetadata.socialBanner, // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: siteMetadata.title,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Event",
+  name: siteMetadata.title,
+  startDate: siteMetadata.eventStartDate,
+  endDate: siteMetadata.eventEndDate,
+  eventAttendanceMode: "https://schema.org/OfflineEventAttendanceMode",
+  eventStatus: "https://schema.org/EventScheduled",
+  location: {
+    "@type": "Place",
+    name: "Snickerpark Stadium",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "CGQC+CCP, Aguleri St",
+      addressLocality: "Independence Layout",
+      postalCode: "400102",
+      addressRegion: "Enu",
+      addressCountry: "NG",
+    },
+  },
+  image: [siteMetadata.socialBanner],
+  description: siteMetadata.description,
+  offers: {
+    "@type": "Offer",
+    url: siteMetadata.eventPaymentUrl,
+    price: "100000",
+    priceCurrency: "NGN",
+    availability: "https://schema.org/InStock",
+    validFrom: "2024-01-21T12:00",
+  },
+  organizer: {
+    "@type": "Organization",
+    name: siteMetadata.eventOrganizer,
+    url: siteMetadata.eventOrganizerUrl,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import ContactForm from "@/components/ContactForm";
+import Hero from "@/components/Hero";
+import Partners from "@/components/Partners";
+import EventProgram from "@/components/program/EventProgram";
+import SpeakersAndHosts from "@/components/speakers-and-hosts";
+import Registration from "@/components/registration";
+import Footer from "@/components/footer";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col text-dark text-lg">
+      <Hero />
+      <EventProgram />
+      <SpeakersAndHosts />
+      <Partners />
+      {/* <ContactForm /> */}
+      <Registration />
+      <Footer />
+    </main>
+  );
+}
