849973804938c11751543d353788d779bcf455f0
Christian Onoh <chibyk5000@gmail.com>
2024-02-23 18:09:44 +0100

    Update footer component with new logo and socials

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "/public/logo.png";
+import Logo from "/public/logo.webp";
+import Socials from "../shared/Socials";
 
 const index = () => {
   return (
-    <footer className="bg-[#262A26] py-12">
+    <footer className="bg-[#00180b] py-24 ">
       <div className="max-w-7xl mx-auto flex flex-col px-5 gap-8 md:px-10 md:flex-row justify-between text-light/70">
-        <Link href="/" className="h-20 w-fit bg-light/80 p-4 rounded-xl">
-          <Image src={Logo} alt="logo" className="h-full w-auto" priority />
+        <Link href="/" className="h-12 w-fit bg-light/80 p-2 rounded-xl">
+          <Image
+            src={Logo}
+            alt="logo"
+            className="h-full object-contain w-auto"
+            priority
+          />
         </Link>
 
-        <div className="text-sm xs:text-base">
+        <Socials className="!bg-transparent !gap-8" />
+
+        <div className=" prose prose-sm text-light/70">
           <p>© 2024 Education Conference. All rights reserved.</p>
           <p>
             Developed by&nbsp;
